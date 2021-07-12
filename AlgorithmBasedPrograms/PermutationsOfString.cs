17d08ae525e524ff3214c06ed7bf3393d5626fb8
saivijayakumar <bollusaivijaykumar212@gmail.com>
2021-07-12 21:04:53 +0530

    [SaiVijayaKumar] Code For Permutation

---
+++ AlgorithmBasedPrograms/PermutationsOfString.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmBasedPrograms
+{
+    class PermutationsOfString
+    {
+        public static void permute(String str, int start, int end)
+        {
+            if (start == end)
+                Console.WriteLine(str);
+            else
+            {
+                for (int i = start; i <= end; i++)
+                {
+                    str = swap(str, start, i);
+                    permute(str, start + 1, end);
+                    str = swap(str, start, i);
+                }
+            }
+        }
+        public static String swap(String a, int i, int j)
+        {
+            char temp;
+            char[] charArray = a.ToCharArray();
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            string s = new string(charArray);
+            return s;
+        }
+    }
+}
--- AlgorithmBasedPrograms/Program.cs
+++ AlgorithmBasedPrograms/Program.cs
@@ -6,15 +6,34 @@ using System.Threading.Tasks;
 
 namespace AlgorithmBasedPrograms
 {
-    /// <summary>
-    /// ------------------>Welcome To Algorithm Based Programs<-----------------
-    /// </summary>
     class Program
     {
+        /// <summary>
+        /// ------------------>Welcome To Algorithm Based Programs<-----------------
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("\t\t--------------->Welcome To Algorithm Based Programs<-----------------\n");
-            Console.Read();
+            Console.WriteLine("-----------{MENU}-----------");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("1.Permutation Of String\n2.");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Enter Your Choice");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.Write("Enter The  String You Want Permute :");
+                    string str = Console.ReadLine();
+                    int length = str.Length;
+                    Console.WriteLine("The Permutations For {0} is ",str);
+                    PermutationsOfString.permute(str, 0, length - 1);
+                    break;
+                default:
+                    Console.WriteLine("Enter Valid Choice");
+                    break;
+            }
+            Console.ReadLine();
         }
     }
 }
