13c9dc2895abe140dda2269e3340173a04b13dc1
saivijayakumar <bollusaivijaykumar212@gmail.com>
2021-07-12 20:54:38 +0530

    [SaiVijayaKumar] Commiting Welcome Message

---
+++ AlgorithmBasedPrograms/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmBasedPrograms
+{
+    /// <summary>
+    /// ------------------>Welcome To Algorithm Based Programs<-----------------
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t--------------->Welcome To Algorithm Based Programs<-----------------\n");
+            Console.Read();
+        }
+    }
+}
